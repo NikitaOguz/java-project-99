d262010ffabf85284040ec2f7b02d528584f64b6
Никита Кобытев <nikitakobytev@mail.ru>
2026-07-01 07:37:30 +0700

    fix Sonar error add Util and ModelGenerator test

--- app/src/test/java/hexlet/code/util/ModelGeneratorTest.java
+++ app/src/test/java/hexlet/code/util/ModelGeneratorTest.java
@@ -6,7 +6,6 @@ import org.instancio.Instancio;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,8 +14,6 @@ class ModelGeneratorTest {
 
     @Autowired
     private ModelGenerator modelGenerator;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Test
     void testUserModel() {
@@ -37,11 +34,5 @@ class ModelGeneratorTest {
         assertThat(status.getId()).isNull();
         assertThat(status.getName()).isNotBlank();
         assertThat(status.getSlug()).isNotBlank();
-    }
-    @Test
-    void testPasswordEncoded() {
-        User user = Instancio.of(modelGenerator.getUserModel()).create();
-
-        assertThat(passwordEncoder.matches("password", user.getPasswordDigest())).isTrue();
     }
 }
